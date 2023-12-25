c606058b1c0135dc275ae6eb63817e24daa4f376
romerachlin <rome.rachlin@post.runi.ac.il>
2023-12-25 15:31:07 +0200

    genthree fix

--- GenThree.java
+++ GenThree.java
@@ -2,9 +2,9 @@ public class GenThree {
 public static void main(String[] args) {
 int min = Integer.parseInt(args[0]);
 int max = Integer.parseInt(args[1]);
-int random_int1 = (int)Math.floor(Math.random() * (max - min + 1) + min);
-int random_int2 = (int)Math.floor(Math.random() * (max - min + 1) + min);
-int random_int3 = (int)Math.floor(Math.random() * (max - min + 1) + min);
+int random_int1 = (int)Math.floor(Math.random() * (max - min) + min);
+int random_int2 = (int)Math.floor(Math.random() * (max - min) + min);
+int random_int3 = (int)Math.floor(Math.random() * (max - min) + min);
 System.out.println(random_int1);
 System.out.println(random_int2);
 System.out.println(random_int3);
