26cde64c6f9a7f2d303868aa4dec3d7c84b3d049
David German <42500275+GermanDavid@users.noreply.github.com>
2023-12-31 14:17:25 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,8 +1,8 @@
 public class Triangle {
 public static void main(String[] args) {
-int Tria = Integer.parseInt(args[0]);
-int Trib = Integer.parseInt(args[1]);
-int Tric = Integer.parseInt(args[2]);
+int tria = Integer.parseInt(args[0]); // #feedback In Java it's a common convention to start variable names with a lowercase letter. 
+int trib = Integer.parseInt(args[1]);
+int tric = Integer.parseInt(args[2]);
 boolean isTri;
 isTri= Tria + Trib > Tric && Tria + Tric > Trib && Tric + Trib > Tria ;
 
