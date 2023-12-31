4626f1acfc836d5a7bafebf9568cd3e99d019891
David German <42500275+GermanDavid@users.noreply.github.com>
2023-12-31 15:20:49 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,7 +1,11 @@
 public class AddTwo {
-public static void main(String[] args) {
-int a = Integer.parseInt(args[0]);
-int b = Integer.parseInt(args[1]);
-System.out.println(a + " + " + b + " = " + (a + b));
-}
+  public static void main(String[] args) {
+    /* #feedback
+    In Java, consistent indentation is key for readability. Typically, 4 spaces are used per indentation level. 
+    This practice helps to clearly distinguish code blocks and enhances code clarity for both the author and others who may read or maintain the code.
+    */
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    System.out.println(a + " + " + b + " = " + (a + b));
+  }
 }
